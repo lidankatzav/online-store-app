a18c6ab1981bbb8267171e00418eed0d1738a89d
lidankatzav <lidankatzav5@gmail.com>
2023-01-06 01:12:16 +0200

    FIFO test

--- server/src/Cache/FIFO-class.ts
+++ server/src/Cache/FIFO-class.ts
@@ -6,12 +6,14 @@ export class FIFO<K,V> extends AbstractCacheAlgo<K,V> implements ICacheAlgo<K,V>
 
     setElement(key: K, value: V): K | undefined {
 
+        // Check if the key already exists
         if(this.getElement(key) !== undefined) {
             this.removeElement(key);
             this.setElement(key,value);
             return key;
         }
 
+        // Check if the cache is full
         if(this.#orderArray.length === this.cacheSize) {
             const removedKey = this.#orderArray.shift();
             this.cacheData.delete(removedKey);
@@ -20,17 +22,21 @@ export class FIFO<K,V> extends AbstractCacheAlgo<K,V> implements ICacheAlgo<K,V>
             return removedKey;
         }
 
+        // Update Key, Value
         this.cacheData.set(key, value);
         this.#orderArray.push(key);
         return;
     }
 
     removeElement(key: K): boolean {
+
+        // Check if the key  exists
         if(this.getElement(key) !== undefined) {
             this.cacheData.delete(key);
             this.#orderArray.splice(this.#orderArray.indexOf(key), 1);
             return true;
         }
+        
         return false;
     }
 
